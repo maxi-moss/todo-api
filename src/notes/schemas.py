08d1d9ce91843de23205b626a75fcec6d7ea9a89
MaxupKindus <genjiisopxd@gmail.com>
2025-08-31 17:32:41 +0200

    Set up Pydantic models for todo and note request/response schemas

--- src/notes/schemas.py
+++ src/notes/schemas.py
@@ -1,13 +1,10 @@
 """Pydantic models"""
 
-from datetime import datetime
-
 from pydantic import BaseModel
 
 
 class NoteModel(BaseModel):
-    id: int
     title: str  # Title of todo 
     content: str  # Full note content
     created_at: str  # Time created
-    edited_at: datetime  # Time last edited
+    edited_at: str  # Time last edited
--- src/todo/schemas.py
+++ src/todo/schemas.py
@@ -1,6 +1,5 @@
 """Pydantic models"""
 
-from datetime import datetime
 from enum import Enum
 
 from pydantic import BaseModel
@@ -12,10 +11,25 @@ class StatusEnum(str, Enum):
     done = "done"
 
 
-class TodoModel(BaseModel):
-    id: int = None  # Optional (primary key)
+class TodoDataModel(BaseModel):
     title: str  # Title of todo 
-    content: str = "No description"  # Longer description (Optional)
-    created_at: datetime  # Time created
-    edited_at: datetime  # Time created
+    content: str = "No description..."  # Longer description (Optional)
+    created_at: str  # Time created
+    edited_at: str  # Time created
     status: StatusEnum  # To do, In progress, Done
+
+
+class TodoCreateRequest(BaseModel):
+    title: str  # Title of todo 
+    content: str = "No description..."  # Longer description (Optional)
+    status: StatusEnum  # To do, In progress, Done
+    
+    
+class TodoUpdateRequest(BaseModel):
+    id: int  # Todo row ID
+    field: str  # Field to edit
+    new_value: str  # New field value
+
+
+class TodoDeleteRequest(BaseModel):
+    id: int  # Todo row ID
